9909374bb29109f7fb5c21ce30152d371e458ec8
Anthony Alexander Atama <100134398+alexanderatama1@users.noreply.github.com>
2023-10-15 11:39:05 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Declare amount and tip percentage variables
+decimal amount, tip;
+
+// Print Header
+Console.WriteLine("Tip Calculator");
+Console.WriteLine("===============");
+
+// Prompt for amount & tip
+Console.WriteLine("Enter amount: ");
+amount = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter tip: ");
+tip = decimal.Parse(Console.ReadLine());
+
+// Clear screen
+Console.Clear();
+
+// Print result with tip amount in green
+Console.Write($"A {tip:p0} tip on {amount:c} is ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write($"{(tip * amount):c}");
+
+// Reset Color
+Console.ResetColor();
