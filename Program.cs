9f7a73c3093416a32b0c44be1864f9eef7eccc7e
Anthony Alexander Atama <alexanderatama1@gmail.com>
2023-10-22 17:01:30 +1100

    A do while loop was added.
    Changes were made to make the app more interactive.

--- Program.cs
+++ Program.cs
@@ -1,24 +1,33 @@
 ﻿// Declare amount and tip percentage variables
 decimal amount, tip;
 
-// Print Header
-Console.WriteLine("Tip Calculator");
-Console.WriteLine("===============");
+do
+{
+    Console.Clear();
+    // Print Header
+    Console.WriteLine("Tip Calculator");
+    Console.WriteLine("===============");
 
-// Prompt for amount & tip
-Console.WriteLine("Enter amount: ");
-amount = decimal.Parse(Console.ReadLine());
+    // Prompt for amount & tip
+    Console.WriteLine("Enter amount: ");
+    amount = decimal.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter tip: ");
-tip = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Enter tip: ");
+    tip = decimal.Parse(Console.ReadLine());
 
-// Clear screen
-Console.Clear();
+    // Clear screen
+    Console.Clear();
 
-// Print result with tip amount in green
-Console.Write($"A {tip:p0} tip on {amount:c} is ");
-Console.ForegroundColor = ConsoleColor.Green;
-Console.Write($"{(tip * amount):c}");
+    // Print result with tip amount in green
+    Console.Write($"A {tip:p0} tip on {amount:c}: is ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write($"{(tip * amount):c}");
 
-// Reset Color
-Console.ResetColor();
+    // Reset Color
+    Console.ResetColor();
+    Console.Write(" Go again? (Y/N): ");
+    if (Console.ReadLine().ToUpper() != "Y")
+    {
+        break;
+    }
+} while (true);
